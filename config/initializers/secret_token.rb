9cae85fd6d203a20e1730c945341a81612491436
Zhai Yingchang <369722924@qq.com>
2014-03-29 10:07:03 +0800

    rails new haha

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Haha::Application.config.secret_key_base = '15b9c8ed065d08bff086c8d3ec26b5979d00ff9e9d0982bc3f409df7f34b8632975b75ad0088045160d2f576da9fbec515b8436a177cb48c96b1493665ffb87e'
