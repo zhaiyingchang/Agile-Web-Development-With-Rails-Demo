bba2cb451947dbd5cccde6aca928fbe57c015d79
Zhai Yingchang <369722924@qq.com>
2014-04-01 16:54:45 +0800

    add cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Haha::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -11,7 +12,7 @@ Haha::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
