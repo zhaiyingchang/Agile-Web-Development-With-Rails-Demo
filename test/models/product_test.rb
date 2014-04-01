c066d5fbab25ab75ddac100432e3d0e2e1525291
Zhai Yingchang <369722924@qq.com>
2014-04-01 16:18:26 +0800

    add functional test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -8,10 +8,7 @@
 #---
 require 'test_helper'
 
-
 class ProductTest < ActiveSupport::TestCase
-  
-  
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -45,13 +42,16 @@ class ProductTest < ActiveSupport::TestCase
                 price:       1,
                 image_url:   image_url)
   end
+
   test "image url" do
     ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
              http://a.b.c/x/y/z/fred.gif }
     bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    
     ok.each do |name|
       assert new_product(name).valid?, "#{name} should be valid"
     end
+
     bad.each do |name|
       assert new_product(name).invalid?, "#{name} shouldn't be valid"
     end
