f70049f39cd0db9ed622d48af9110e8a9b78b526
Zhai Yingchang <369722924@qq.com>
2014-04-02 10:32:54 +0800

    link cart, lineitem and product

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,12 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 # 
@@ -20,4 +26,15 @@ class Product < ActiveRecord::Base
   def self.latest
     Product.order(:updated_at).last
   end
+  private
+
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items present')
+        return false
+      end
+    end
 end
